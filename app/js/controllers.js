1e3771d933c0f2d73ebc0fbf8ae26de49dae3a1d
vipwoody <vipwoody@gmail.com>
2013-03-21 09:41:25 -0400

    added new controllers, connects with mongolab now

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,32 @@
 'use strict';
 
 /* Controllers */
+
+function PhoneListCtrl($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
+
+  $scope.orderProp = 'age';
+}
+
+//PhoneListCtrl.$inject = ['$scope', '$http'];
+
+
+function PhoneDetailCtrl($scope, $routeParams) {
+  $scope.phoneId = $routeParams.phoneId;
+}
+
+//PhoneDetailCtrl.$inject = ['$scope', '$routeParams'];
+
+function CityListCtrl($scope, $http){
+	$http.get('https://api.mongolab.com/api/1/databases/world/collections/cities?apiKey=PpXY7fPp3frW1XD6lTNCUOuEDfbi40ES').success(function(data){
+		$scope.cities = data;
+
+	});
+	$scope.size = 20;
+	$scope.ordered = 'pop';
+
+}
+
+//CityListCtrl.$inject = ['$scope', '$http'];
