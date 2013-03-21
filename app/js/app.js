50289233b69de724d0637c120e01d5b3bcf0f1dc
vipwoody <vipwoody@gmail.com>
2013-03-21 09:40:45 -0400

    added some routing, changes views according to url

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
 /* App Module */
+
+
+angular.module('phonecat', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
+      when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
+      when('/home', {templateUrl: 'partials/home.html'}).
+	  when('/contact', {templateUrl: 'partials/contact.html', controller: CityListCtrl}).
+	  when('/about', {templateUrl: 'partials/about.html'}).
+      otherwise({redirectTo: '/phones'});
+}]);
+
+
+
+// angular.module('clogged', [])
+// 	.config(['$routerProvider', function($routeProvider){
+// 		$routeProvider
+// 		.when('/home', {templateUrl: 'partials/home.html'})
+// 		.when('/contact', {templateUrl: 'partials/contact.html'})
+// 		.when('/about', {templateUrl: 'partials/about.html'})
+// 		.otherwise({redirectTo: '/home'});
+// 	}]);
